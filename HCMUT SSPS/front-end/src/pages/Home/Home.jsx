a5a62c064d5691254ee1dc74b69cbfd55cd4ffda
Khoa Tran <khoatrandd.139@gmail.com>
2024-11-21 15:40:47 +0700

    adsalfasdf

--- HCMUT SSPS/front-end/src/pages/Home/Home.jsx
+++ HCMUT SSPS/front-end/src/pages/Home/Home.jsx
@@ -1,3 +1,39 @@
+// import React from "react";
+// import { Navbar } from "../../components/Navbar/Navbar";
+// import { Footer } from "../../components/Footer/Footer";
+// import BackgroundSVG from "../../assets/background.svg"; // Path to the SVG
+// import "./home.css"
+
+// function Home() {
+//     return (
+//         <div className="home">
+//             <div className="div-2">
+//                 <Footer />
+//                 <Navbar property1="Registered User"/>
+//                 <div className="frame-3">
+//                     <div className="text-wrapper-2">Tải tài liệu</div>
+
+//                     <div className="frame-wrapper">
+//                         <div className="frame-4">
+//                             <div className="button-4">
+//                                 <FilePlus className="file-plus" />
+//                                 <p className="button-5">Chọn tệp từ thiết bị</p>
+//                             </div>
+
+//                             <p className="text-wrapper-3">Hoặc kéo thả tệp của bạn vào đây</p>
+//                         </div>
+//                     </div>
+//                 </div>
+//             </div>
+//         </div>
+//     );
+// }
+
+
+// export default Home;
+
+
+
 import React from "react";
 import { Navbar } from "../../components/Navbar/Navbar";
 import { Footer } from "../../components/Footer/Footer";
