182b6a08ea08b7482e18ab50ff5de80a28a34ce2
unknown <tnakhoi6@gmail.com>
2024-11-22 07:22:37 +0700

    commit crime

--- HCMUT SSPS/front-end/src/pages/PrintingComplete/PrintingComplete.jsx
+++ HCMUT SSPS/front-end/src/pages/PrintingComplete/PrintingComplete.jsx
@@ -3,3 +3,4 @@ import { Navbar } from "../../components/Navbar/Navbar";
 import { Footer } from "../../components/Footer/Footer";
 import BackgroundSVG from "../../assets/background.svg"; // Path to the SVG
 import "./printingComplete.css"
+
