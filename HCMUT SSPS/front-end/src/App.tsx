d2ce3a0f5a2e4288f9e9169889566794db6c6e9e
Khoa Tran <khoatrandd.139@gmail.com>
2024-11-22 10:08:14 +0700

    home

--- HCMUT SSPS/front-end/src/App.tsx
+++ HCMUT SSPS/front-end/src/App.tsx
@@ -1,15 +1,18 @@
 // import { Footer } from "./components/Footer/Footer";
 // import { Navbar } from "./components/Navbar/Navbar";
 import Home from "./pages/Home/Home";
-
 import ChoosePrinter from "./pages/ChoosePrinter/ChoosePrinter";
 import Configurations from "./pages/Configurations/Configurations";
 import UserLog from "./pages/UserLog/UserLog";
+import { Navbar } from "./components/Navbar/Navbar";
+import { Footer } from "./components/Footer/Footer";
 
 function App() {
     return (
         <div>
-            <Configurations/>
+            {/* <Configurations/> */}
+            {/* <ChoosePrinter /> */}
+            <Home/>
         </div>
     );
 }
