2525c539e185d58d4f64dcea7f4afea50fd75fa8
Monarl <vyhspro12345@gmail.com>
2024-11-23 09:25:47 +0700

    Add Manage_Printer

--- HCMUT SSPS/front-end/src/App.tsx
+++ HCMUT SSPS/front-end/src/App.tsx
@@ -2,11 +2,12 @@
 // import { Navbar } from "./components/Navbar/Navbar";
 import Home from "./pages/Home/Home";
 import Hist_Admin from "./pages/Printing_History(Admin)/hist_Admin"
+import Manage_Printer from "./pages/Manage_Printer/Manage_Printer"
 
 function App() {
     return (
         <div>
-            <Hist_Admin/>
+            <Manage_Printer/>
         </div>
     );
 }
