d63e3426e0d486f2f6117f3475534107c5ebe651
Monarl <vyhspro12345@gmail.com>
2024-12-02 12:54:49 +0700

    add route

--- HCMUT SSPS/front-end/src/App.tsx
+++ HCMUT SSPS/front-end/src/App.tsx
@@ -1,15 +1,20 @@
-// import { Footer } from "./components/Footer/Footer";
-// import { Navbar } from "./components/Navbar/Navbar";
-import Home from "./pages/Home/Home";
-import Hist_Admin from "./pages/Printing_History(Admin)/hist_Admin"
-import Manage_Printer from "./pages/Manage_Printer/Manage_Printer"
-import Buy_Page from "./pages/Buy_Page/Buy_Page";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import Hist_Admin from './pages/Printing_History(Admin)/hist_Admin';
+import Manage_Printer from './pages/Manage_Printer/Manage_Printer';
+import Buy_Page from './pages/Buy_Page/Buy_Page';
 
 function App() {
     return (
-        <div>
-            <Buy_Page/>
-        </div>
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/history-admin" element={<Hist_Admin />} />
+                <Route path="/manage-printer" element={<Manage_Printer />} />
+                <Route path="/buy" element={<Buy_Page />} />
+            </Routes>
+        </Router>
     );
 }
 
