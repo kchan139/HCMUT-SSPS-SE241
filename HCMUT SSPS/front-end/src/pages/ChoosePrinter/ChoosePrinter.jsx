767d8cbc76f36c63cff269a8a76d9f93ade11092
Khoa Tran <khoatrandd.139@gmail.com>
2024-12-04 11:44:57 +0700

    added printer to choose printer

--- HCMUT SSPS/front-end/src/pages/ChoosePrinter/ChoosePrinter.jsx
+++ HCMUT SSPS/front-end/src/pages/ChoosePrinter/ChoosePrinter.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 import { Navbar } from "../../components/Navbar/Navbar";
 import { Footer } from "../../components/Footer/Footer";
 import BackgroundSVG from "../../assets/background.svg";
@@ -7,6 +7,7 @@ import { IconPrinter } from "icons";
 import { ButtonGroup, Navigation } from "primitives";
 import { Button } from "primitives";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 function NavigationButtons(){
     const navigate = useNavigate();
@@ -19,8 +20,23 @@ function NavigationButtons(){
 }
 
 function ChoosePrinter(){
+    // State to hold the printers data
+    const [printers, setPrinters] = useState([]);
+    
+    // Fetch printers when the component mounts
+    useEffect(() => {
+        // Fetch data from Flask backend
+        axios.get("http://127.0.0.1:5000/api/printers")
+            .then(response => {
+                // Set the printers data to state
+                setPrinters(response.data);
+            })
+            .catch(error => {
+                console.error("Error fetching printers:", error);
+            });
+    }, []);
 
-    return(
+    return (
         <div>
             <Navbar property="Registered User" />
             <div className="background_choosePrinter">
@@ -28,35 +44,29 @@ function ChoosePrinter(){
                     src={BackgroundSVG}
                     alt="Background_choosePrinter"
                     className="background-image_choosePrinter"
-                />{
-                    <div className="content_choosePrinter">
-                        <div className="border_choosePrinter">
-                            <div className="header_choosePrinter">Máy in khả dụng</div>
-                            <div className="bar_choosePrinter"><IconPrinter size="32" className="printerIcon_choosePrinter"/>
-                                <span className="line_choosePrinter">A4 - 402: Printer 1</span>
-                                <input type="radio" name="printer" className="radioButton_choosePrinter"/>
+                />
+                <div className="content_choosePrinter">
+                    <div className="border_choosePrinter">
+                        <div className="header_choosePrinter">Máy in khả dụng</div>
+
+                        {/* Dynamically render printers */}
+                        {printers.map(printer => (
+                            <div key={printer.id} className="bar_choosePrinter">
+                                <IconPrinter size="32" className="printerIcon_choosePrinter" />
+                                <span className="line_choosePrinter">
+                                    {printer.name}
+                                </span>
+                                <input type="radio" name="printer" className="radioButton_choosePrinter" />
                             </div>
-                            <div className="bar_choosePrinter"><IconPrinter size="32" className="printerIcon_choosePrinter"/>
-                                <text className="line_choosePrinter">A4 - 402: Printer 2</text>
-                                <input type="radio" name="printer" className="radioButton_choosePrinter"/>
-                            </div>
-                            <div className="bar_choosePrinter"><IconPrinter size="32" className="printerIcon_choosePrinter"/>
-                                <text className="line_choosePrinter">A4 - 402: Printer 3</text>
-                                <input type="radio" name="printer" className="radioButton_choosePrinter"/>
-                            </div>
-                            <div className="bar_choosePrinter"><IconPrinter size="32" className="printerIcon_choosePrinter"/>
-                                <text className="line_choosePrinter">A4 - 402: Printer 4</text>
-                                <input type="radio" name="printer" className="radioButton_choosePrinter"/>
-                            </div>
-                            
-                            <NavigationButtons/>
-                        </div>
+                        ))}
+
+                        <NavigationButtons />
                     </div>
-                }
+                </div>
             </div>
             <Footer />
         </div>
-    )
+    );
 }
 
 export default ChoosePrinter
